dc71bc0dcd87ea722352ab6e8301c4a1bd0e8bbe
Greyson Mainor <gbmainor98@outlook.com>
2021-01-30 11:59:18 -0500

    added some background and margins to my 5 day forecast.  Also adding ajax call.

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 $(document).ready(function(){
-var searchButton = $("#search-button")
-var searchCity = $("#search-city")
-var currentTemp = $("#temp")
-var currentWind = $("#wind")
-var currentHumidity = $("#humidity")
-var currentUv = $("#uv")
-var currentCity = $("#current-city")
+var searchButton = $("#search-button");
+var searchCity = $("#search-city");
+var currentTemp = $("#temp");
+var currentWind = $("#wind");
+var currentHumidity = $("#humidity");
+var currentUv = $("#uv");
+var currentCity = $("#current-city");
+var city = "";
 
+var apiKey = "3d94d734329ac2e25782373114d9c091";
 
+$("#search-button").on("click",getWeather)
+
+function getWeather(event){
+    event.preventDefault();
+city = searchCity.val();
+currentWeather(city);
+}
+
+function currentWeather(city){
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response)
+    })
+}
 
 
 
