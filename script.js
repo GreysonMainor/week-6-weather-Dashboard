38f36e29392fc04860247ca788a75110833c7751
Greyson Mainor <gbmainor98@outlook.com>
2021-01-30 04:00:11 -0500

    added variables in my script for all of my IDs on my html.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
+var searchButton = $("#search-button")
+var searchCity = $("#search-city")
+var currentTemp = $("#temp")
+var currentWind = $("#wind")
+var currentHumidity = $("#humidity")
+var currentUv = $("#uv")
+var currentCity = $("#current-city")
 
 
 
 
-    
+
 });
